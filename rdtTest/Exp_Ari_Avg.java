72efba83a16ae512b9cfc2f0307cabcb37cfd8be
Andrew Pirkle <andrewcpirkle@gmail.com>
2016-09-25 20:23:04 -0400

    did most of it

--- rdtTest/Exp_Ari_Avg.java
+++ rdtTest/Exp_Ari_Avg.java
@@ -1,3 +1,5 @@
+package rdtTest;
+
 import java.io.File;  
 import java.io.PrintWriter;
 import java.io.FileNotFoundException;  
@@ -8,27 +10,55 @@ import java.util.ArrayList;
 import java.io.IOException;  
 import java.util.Random;
 
-public class Exp_Ari_Avg{  	 
+public class Exp_Ari_Avg{ 
 	 public static double expAvg(double preExpAvg, int newSample){
 	 	// To be completed...
-	 	return 0;
+		 // (1 - a)*EstimatedRTT + a*SampleRTT
+		 double a = .125;
+		 preExpAvg = (1 - a)*preExpAvg + (a*newSample);
+	 	return preExpAvg;
 	 }
 	 
-	 public static double ariAvg(double preAriAvg, int newSample, int n){
+	 public static double ariAvg(double preAriAvg, int newSample, int n, double alltheshit){
 		// To be completed...
-	 	return 0;
+		 // (SampleRTT1 + SampleRTT2 +...+ SampleRTTn)/n
+		 preAriAvg =  alltheshit/n;
+	 	return preAriAvg;
 	 }
 	 
 	 public static void sampleRTT2EstimatedRTT(Scanner in, PrintWriter out)throws IOException{ 
 	 	// To be completed...
+	//	Generate initial random number between 0 - 100
+		 Random rn = new Random();
+			double EstRTT1 = rn.nextInt(101);
+			double EstRTT2 = rn.nextInt(101);
+			int n = 1;
+			double alltheshit = 0.0;
+	// get time from text file		
+			while(in.hasNextLine()){
+				String pattern = in.nextLine();
+				Pattern p = Pattern.compile("time=(.*?)ms");
+				Matcher m = p.matcher(pattern);
+				while(m.find()){
+					int time = Integer.parseInt(m.group(1));
+					EstRTT1 = expAvg(EstRTT1, time);
+					alltheshit += time;
+					EstRTT2 = ariAvg(EstRTT2, time, n, alltheshit);
+					n++;
+					System.out.printf(time +"\t"+ EstRTT1 + "\t" + EstRTT2 + "\n");
+					out.printf("%d \t\t %.3f \t %.3f", time,EstRTT1,EstRTT2);
+					out.println();
+				}
+			}
 	 }
 	 public static void main (String[] args){ 
 	 	try{
-			
-			Scanner fileScanner = new Scanner(new File("data.txt")); 
+			Scanner fileScanner = new Scanner(new File("data.txt"));
 			PrintWriter filePrinter = new PrintWriter(new File("output.txt"));
-			
-			filePrinter.print("SampleRTTs\tExpAvgs\tAriAvgs");
+//			while(fileScanner.hasNextLine()){
+//				System.out.println(fileScanner.nextLine());
+//			}
+			filePrinter.print("SampleRTTs\tExpAvgs\t\tAriAvgs");
 			filePrinter.println();
 			
 			sampleRTT2EstimatedRTT(fileScanner, filePrinter);
